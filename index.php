ff336d1458b2dd0c7f2f866f084e04480598a408
Paweł__ <pawelkoc681@gmail.com>
2023-03-09 11:27:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,37 +20,33 @@
     $sql_show = "SELECT * FROM $tablename WHERE postac = $postac";
     $sql_update ="UPDATE $tablename SET postac = $postac , poziom = $poziom , rasa = $rasa, klasa = $klasa , swiat = $swiat WHERE postac = $postac ";
     $sql_delete = "DELETE FROM $tablename WHERE postac = $postac";
-    $sql_add = "INSERT INTO $tablename(`postac`, `poziom`,`rasa`,`klasa`,`swiat` VALUES ($postac,$poziom,$rasa,$klasa,$swiat)";
+    $sql_add = "INSERT INTO $tablename(`postac`, `poziom`,`rasa`,`klasa`,`swiat`) VALUES (`$postac`,`$poziom`,`$rasa`,`$klasa`,`$swiat`)";
 
 
 
     if(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING) == 'add'){
-        if(mysqli_query($connection,$sql_add)){
-            echo 'added succesfully';
-        } else {
-            echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
-        }
-    }elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING == 'update'){
-        if(mysqli_query($connection,$sql_update)){
-            echo ' updated succesfully';
-        } else {
-            echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
-        }
-    }elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING == 'show'){
-        if($result = mysqli_query($connection,$sql_show)){
-            foreach $result as $row {
+
+        mysqli_query($connection,$sql_add);
+        
+    }
+    elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING) == 'update'){
+
+        mysqli_query($connection,$sql_update);
+
+    }
+    elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING) == 'show'){
+        $result = mysqli_query($connection,$sql_show);
+            foreach ($result as $row) {
                 echo $row;
             }
-        }else{
-            echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
-        }
-    }elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING == 'delete'){
-        if(mysqli_query($connection,$sql_delete)){
-            echo 'deleted succesfully';
-        }else{
-            echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
-        }
-    }else {
+    }
+    elseif(filter_input(INPUT_POST, 'select', FILTER_SANITIZE_STRING) == 'delete'){
+
+        mysqli_query($connection,$sql_delete);
+           
+    }
+    else {
         echo 'Imput all the information about your charakter';
+        // echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
     }
 ?>
